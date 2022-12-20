5777328da4700dce80dfad6b6cb6e1c2341b9212
Douglas Ferreira <dodofp.df@gmail.com>
2022-12-20 15:43:59 -0300

    feat: styled components and custom theme

--- App.tsx
+++ App.tsx
@@ -1,13 +1,31 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import theme from 'src/styles/theme'
+import { ThemeProvider } from 'styled-components'
+import { 
+  useFonts,
+  Inter_400Regular, 
+  Inter_500Medium
+} from '@expo-google-fonts/inter'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_500Medium
+  })
+
+  if(!fontsLoaded) {
+    return <Text>Carregando</Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,47 @@
+export default {
+  colors: {
+    gray: {
+      10: '#E0E0E6',
+      20: '#C1C1CD',
+      30: '#A2A2B5',
+      40: '#83839C',
+      50: '#666680',
+      60: '#4E4E61',
+      70: '#353542',
+      80: '#1C1C23',
+      100: '#0E0E12',
+    },
+    primary: {
+      0: '#E4D3FF',
+      5: '#C9A7FF',
+      10: '#AD7BFF',
+      20: '#924EFF',
+      100: '#5E00F5',
+      500: '#7722FF'
+    },
+    accent_primary: {
+      0: '#FFD2CC',
+      50: '#FFA699',
+      100: '#FF7966'
+    },
+    accent_secondary: {
+      50: '#7DFFEE',
+      100: '#00FAD9'
+    }
+  },
+  fontSize: {
+    display: 72,
+    headline1: 56,
+    headline2: 40,
+    headline3: 32,
+    headline4: 24,
+    headline5: 20,
+    headline6: 16,
+    headline7: 14,
+    headline8: 12,
+  },
+  fonts: {
+    primary_400: 'Inter_400Regular',
+    primary_500: 'Inter_500Medium',
+  }
+}
